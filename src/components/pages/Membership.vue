a99830cb4b749f21322f2b6aec4c0364ed3f129a
OG'ABEK <shomoyxhacker007@mail.ru>
2022-06-27 07:45:45 -0500

    show more modal added

--- src/components/pages/Membership.vue
+++ src/components/pages/Membership.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="px-5">
-    <div class="flex justify-between items-center mb-5">
-      <h3 class="text-2xl font-extrabold ml-2 mb-3">A'zolar</h3>
+    <div class="mb-5 flex items-center justify-between">
+      <h3 class="ml-2 mb-3 text-2xl font-extrabold">A'zolar</h3>
       <div>
-        <div class="relative inline-block mr-3">
+        <div class="relative mr-3 inline-block">
           <span class="absolute inset-y-0 left-0 flex items-center pl-2">
             <button type="submit" class="focus:shadow-outline p-4 focus:outline-none">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="h-5 w-5 text-gray-500"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -11,17 +11,136 @@
           </span>
           <input type="search" name="search" class="w-96 rounded-lg border border-slate-300 bg-white py-1.5 pl-16 text-lg text-slate-500 outline-none focus:bg-slate-200 focus:outline-none" placeholder="Izlash..." autocomplete="off" />
         </div>
-        <button type="submit" class="mr-3 text-gray-900 bg-white border bordeer-slare-300 hover:bg-slate-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Saralash</button>
-        <button type="submit" class="mx-1 text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">A'zo qo'shish</button>
+        <button type="submit" class="bordeer-slare-300 mr-3 w-full rounded-lg border bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-slate-200 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">Saralash</button>
+        <button type="submit" class="mx-1 w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">A'zo qo'shish</button>
       </div>
     </div>
     <div class="grid grid-cols-5 gap-4">
       <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md">
         <img src="../../assets/images/bg-login.jpg" alt="#" class="h-auto w-full" />
-        <div class="flex flex-col justify-center p-5 text-center">
-          <h3 class="text-md font-extrabold mb-2">Jumaniyozov Surojiddin</h3>
+        <div class="flex flex-col justify-center p-5 text-center" x-data="{open: false}">
+          <h3 class="text-md mb-2 font-extrabold">Jumaniyozov Surojiddin</h3>
           <p class="text-medium mb-3 text-gray-600">+998 (93) 747-59-95</p>
-          <button class="rounded-md mx-14 bg-white py-2 px-5 border border-slate-300 text-gray-500 hover:bg-slate-100 flex justify-center items-center">Batafsil <ArrowRightIcon class="text-xl ml-3"/></button>
+          <button x-on:mouseenter="open = true" x-on:mouseleave="open = false" @click="openModal()" class="mx-14 flex items-center justify-center rounded-md border border-slate-300 bg-white py-2 px-5 text-gray-500 transition-all duration-200 ease-in hover:bg-slate-100">Batafsil <ArrowRightIcon x-show="open" class="ml-3 animate-pulse text-xl transition-all duration-500 ease-in" /></button>
+        </div>
+      </div>
+    </div>
+    <!-- Modal -->
+    <div v-show="isModalOpen" class="fixed top-0 right-0 left-0 z-50 h-modal w-full overflow-y-auto overflow-x-hidden backdrop-brightness-50 md:inset-0 md:h-full">
+      <div class="relative top-1/2 left-1/2 h-full w-full max-w-7xl -translate-x-1/2 -translate-y-1/2 p-4 md:h-auto">
+        <div class="relative rounded-lg bg-white shadow-lg dark:bg-gray-700">
+          <div class="flex items-start justify-between rounded-t border-b p-4 dark:border-gray-600">
+            <div class="text-xl font-semibold text-gray-900 dark:text-white">
+              <div class="flex items-center">
+                <img src="../../assets/images/bg-login.jpg" alt="Avatar" class="ml-2 h-20 w-20 rounded-full" />
+                <div class="ml-4 pr-2">
+                  <h3>Admin Adminjonov</h3>
+                  <h5 class="text-sm">+998(99) 123-45-67</h5>
+                  <h6 class="text-sm text-gray-500">12.02.2022</h6>
+                </div>
+              </div>
+            </div>
+            <button type="button" @click="closeModal()" class="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button>
+          </div>
+          <div class="p-6">
+            <h2 class="font-bold text-xl">To'lovlar hisoboti</h2>
+            <hr class="mb-2">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table class="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+                <thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" class="px-6 py-3">Product name</th>
+                    <th scope="col" class="px-6 py-3">Color</th>
+                    <th scope="col" class="px-6 py-3">Category</th>
+                    <th scope="col" class="px-6 py-3">Price</th>
+                    <th scope="col" class="px-6 py-3">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="border-b odd:bg-white even:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">Apple MacBook Pro 17"</th>
+                    <td class="px-6 py-4">Sliver</td>
+                    <td class="px-6 py-4">Laptop</td>
+                    <td class="px-6 py-4">$2999</td>
+                    <td class="px-6 py-4 text-right">
+                      <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                    </td>
+                  </tr>
+                  <tr class="border-b odd:bg-white even:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">Microsoft Surface Pro</th>
+                    <td class="px-6 py-4">White</td>
+                    <td class="px-6 py-4">Laptop PC</td>
+                    <td class="px-6 py-4">$1999</td>
+                    <td class="px-6 py-4 text-right">
+                      <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                    </td>
+                  </tr>
+                  <tr class="border-b odd:bg-white even:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">Magic Mouse 2</th>
+                    <td class="px-6 py-4">Black</td>
+                    <td class="px-6 py-4">Accessories</td>
+                    <td class="px-6 py-4">$99</td>
+                    <td class="px-6 py-4 text-right">
+                      <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <h2 class="font-bold text-xl mt-5">Kelib-ketishlar hisoboti</h2>
+            <hr class="mb-2">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table class="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+                <thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" class="px-6 py-3">Product name</th>
+                    <th scope="col" class="px-6 py-3">Color</th>
+                    <th scope="col" class="px-6 py-3">Category</th>
+                    <th scope="col" class="px-6 py-3">Price</th>
+                    <th scope="col" class="px-6 py-3">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="border-b odd:bg-white even:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">Apple MacBook Pro 17"</th>
+                    <td class="px-6 py-4">Sliver</td>
+                    <td class="px-6 py-4">Laptop</td>
+                    <td class="px-6 py-4">$2999</td>
+                    <td class="px-6 py-4 text-right">
+                      <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                    </td>
+                  </tr>
+                  <tr class="border-b odd:bg-white even:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">Microsoft Surface Pro</th>
+                    <td class="px-6 py-4">White</td>
+                    <td class="px-6 py-4">Laptop PC</td>
+                    <td class="px-6 py-4">$1999</td>
+                    <td class="px-6 py-4 text-right">
+                      <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                    </td>
+                  </tr>
+                  <tr class="border-b odd:bg-white even:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">Magic Mouse 2</th>
+                    <td class="px-6 py-4">Black</td>
+                    <td class="px-6 py-4">Accessories</td>
+                    <td class="px-6 py-4">$99</td>
+                    <td class="px-6 py-4 text-right">
+                      <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="flex items-center justify-end space-x-2 rounded-b border-t border-gray-200 p-6 dark:border-gray-600">
+            <button type="button" @click="closeModal()" class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Yopish</button>
+          </div>
         </div>
       </div>
     </div>
@@ -29,8 +148,16 @@
 </template>
 
 <script setup>
-import ArrowRightIcon from '../../assets/icons/ArrowRightIcon.vue';
+import ArrowRightIcon from '../../assets/icons/ArrowRightIcon.vue'
+import { ref } from 'vue'
 
+const isModalOpen = ref(false)
 
+const openModal = () => {
+  isModalOpen.value = true
+}
 
+const closeModal = () => {
+  isModalOpen.value = false
+}
 </script>
