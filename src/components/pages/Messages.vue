e75b64515864ba844f6e3e752d84efa22067fb46
Surojiddin <isurojiddin@gmail.com>
2022-07-20 11:55:36 +0500

    messages filter

--- src/components/pages/Messages.vue
+++ src/components/pages/Messages.vue
@@ -3,10 +3,28 @@
     <div class="h-full px-5">
       <div class="flex items-center justify-between mb-5">
         <h3 class="mb-3 ml-2 text-2xl font-extrabold dark:text-gray-300">SMS xabarlar</h3>
-        <button class="hidden w-full px-5 py-2 text-center text-gray-900 bg-white border rounded-lg border-slate-300 hover:bg-slate-200 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-0 dark:bg-blue-600 dark:text-gray-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto lg:inline-block">
-          <FunnelIcon class="inline-block mr-1 text-lg" />
-          Saralash
-        </button>
+        <div class="relative hidden lg:flex lg:justify-between lg:items-center">
+          <div class="flex items-center mr-3">
+            <div class="relative">
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <span class="text-sm text-gray-600 dark:text-gray-300"> dan </span>
+              </div>
+              <input v-model.lazy="filterData.filterDateFrom" name="start" type="datetime-local" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-11 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+            <ArrowRightIcon class="mx-2 text-gray-500" />
+            <div class="relative">
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <span class="text-sm text-gray-600 dark:text-gray-300"> gacha </span>
+              </div>
+              <input v-model.lazy="filterData.filterDateTo" name="end" type="datetime-local" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-14 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+          </div>
+          <button @click="openFilter = !openFilter" class="border-slate-300 w-full rounded-lg border bg-white px-5 py-2 text-center text-gray-900 hover:bg-slate-200 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto dark:border-0 dark:text-gray-300"><FunnelIcon class="mr-1 inline-block text-lg" /> Saralash</button>
+          <div v-if="openFilter" ref="filterDropdown" class="bg-white dark:bg-gray-800 border dark:border-gray-600 absolute top-11 right-0 z-30 dark:text-gray-300 w-1/4 mt-2 rounded-lg">
+            <div @click="openFilter = false; filterData.typeBy = 'send-code'" class="px-3 py-2 border-b dark:border-gray-600 dark:hover:bg-gray-700 cursor-pointer">Aktivlash kodlari</div>
+            <div @click="openFilter = false; filterData.typeBy = 'reminder'" class="px-3 py-2 dark:hover:bg-gray-700 cursor-pointer">Eslatma xabarlari</div>
+          </div>
+        </div>
       </div>
       <div v-show="messages.length > 0" :class="{ 'overflow-hidden': total / 10 <= 1 }" class="grid grid-cols-1 overflow-x-auto border rounded-lg shadow-lg messages-wrapper messages-table-h border-slate-200 dark:border-gray-600">
         <table class="w-full divide-y divide-gray-300 dark:divide-gray-600">
@@ -32,16 +50,32 @@
 </template>
 
 <script setup>
+import SpinIcon from '../../assets/icons/SpinIcon.vue'
 import FunnelIcon from '../../assets/icons/FunnelIcon.vue'
 import MessagesInfo from './Messages/MessagesInfo.vue'
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { onClickOutside } from '@vueuse/core'
 import authHeader from '../../services/auth-header.js'
-import SpinIcon from '../../assets/icons/SpinIcon.vue'
 
 const target = ref('.messages-wrapper')
 const distance = ref(200)
 const messages = ref([])
 const total = ref(0)
+
+// Filter By
+const openFilter = ref(false)
+const filterDropdown = ref(null)
+
+onClickOutside(filterDropdown, () => {
+  if (openFilter.value) openFilter.value = false
+})
+
+// load default
+const filterData = reactive({
+  typeBy: null,
+  filterDateFrom: null,
+  filterDateTo: null,
+})
 
 let page = 0
 const loadMessages = async ($state) => {
@@ -49,6 +83,8 @@ const loadMessages = async ($state) => {
   if (!(total.value / 10 + 1 < page && total.value !== 0)) {
     try {
       const response = await fetch('http://localhost:9000/message/' + page, {
+        method: 'POST',
+        body: JSON.stringify(filterData),
         headers: authHeader(),
       })
       const json = await response.json()
@@ -68,6 +104,58 @@ const isMessagesEmpty = ref(false)
 setTimeout(() => {
   isMessagesEmpty.value = messages.value.length === 0
 }, 700)
+
+// load filtered
+const loadFiltered = async () => {
+  page++
+  if (!(total.value / 10 + 1 < page && total.value !== 0)) {
+    try {
+      const response = await fetch('http://localhost:9000/message/' + page, {
+        method: 'POST',
+        body: JSON.stringify(filterData),
+        headers: authHeader(),
+      })
+      const json = await response.json()
+      total.value = json.total
+      setTimeout(() => {
+        messages.value.push(...json.messages)
+      }, 500)
+    } catch (error) {
+      notify.warning({
+        message: error,
+        position: 'bottomLeft',
+      })
+    }
+  }
+}
+
+const refresher = () => {
+  page = 0
+  total.value = 0
+  messages.value = []
+  loadFiltered()
+  setTimeout(() => {
+    isMessagesEmpty.value = messages.value.length === 0
+  }, 700)
+}
+
+watch(
+  () => filterData.typeBy,
+  () => refresher(),
+  { deep: true }
+)
+
+watch(
+  () => filterData.filterDateFrom,
+  () => refresher(),
+  { deep: true }
+)
+
+watch(
+  () => filterData.filterDateTo,
+  () => refresher(),
+  { deep: true }
+)
 </script>
 
 <style scoped>
