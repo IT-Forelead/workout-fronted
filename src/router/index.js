fad6d96441df9f5ebe9e9ee004ab86991a13f699
OG'ABEK <shomoyxhacker007@mail.ru>
2022-07-26 05:00:59 -0500

    router method typo changed

--- src/router/index.js
+++ src/router/index.js
@@ -17,68 +17,68 @@ const routes = [
     path: '/dashboard',
     name: 'Asosiy sahifa',
     component: () => import('../components/pages/Main.vue'),
-    beforeEnter: navidagtionGuard('client'),
+    beforeEnter: navigationGuard('client'),
   },
   {
     path: '/members',
     name: "A'zolar",
     component: () => import('../components/pages/Membership.vue'),
-    beforeEnter: navidagtionGuard('client'),
+    beforeEnter: navigationGuard('client'),
   },
   {
     path: '/payments',
     name: "To'lovlar",
     component: () => import('../components/pages/Payments.vue'),
-    beforeEnter: navidagtionGuard('client'),
+    beforeEnter: navigationGuard('client'),
   },
   {
     path: '/arrival',
     name: 'Kelib-ketishlar',
     component: () => import('../components/pages/Arrival.vue'),
-    beforeEnter: navidagtionGuard('client'),
+    beforeEnter: navigationGuard('client'),
   },
   {
     path: '/messages',
     name: 'SMS xabarlar',
     component: () => import('../components/pages/Messages.vue'),
-    beforeEnter: navidagtionGuard('all'),
+    beforeEnter: navigationGuard('all'),
   },
   {
     path: '/settings',
     name: 'Sozlamalar',
     component: () => import('../components/pages/Settings.vue'),
-    beforeEnter: navidagtionGuard('client'),
+    beforeEnter: navigationGuard('client'),
   },
   {
     path: '/profile',
     name: 'Profil',
     component: () => import('../components/pages/Profile.vue'),
-    beforeEnter: navidagtionGuard('client'),
+    beforeEnter: navigationGuard('client'),
   },
   // Admin routes
   {
     path: '/admin-dashboard',
     name: 'Admin Dashboard',
     component: () => import('../components/pages/admin/AdminHome.vue'),
-    beforeEnter: navidagtionGuard('admin'),
+    beforeEnter: navigationGuard('admin'),
   },
   {
     path: '/unverified-clients',
     name: 'UnverifiedClients',
     component: () => import('../components/pages/admin/UnverifiedClients.vue'),
-    beforeEnter: navidagtionGuard('admin'),
+    beforeEnter: navigationGuard('admin'),
   },
   {
     path: '/clients',
     name: 'Clients',
     component: () => import('../components/pages/admin/Clients.vue'),
-    beforeEnter: navidagtionGuard('admin'),
+    beforeEnter: navigationGuard('admin'),
   },
   {
     path: '/admin-profile',
     name: 'Admin Profil',
     component: () => import('../components/pages/Profile.vue'),
-    beforeEnter: navidagtionGuard('admin'),
+    beforeEnter: navigationGuard('admin'),
   },
   {
     path: '/:pathMach(.*)*',
@@ -103,7 +103,7 @@ router.beforeEach((to, from, next) => {
   }
 })
 
-function navidagtionGuard(role) {
+function navigationGuard(role) {
   if (role === 'all'){
     return () => {
       return true
