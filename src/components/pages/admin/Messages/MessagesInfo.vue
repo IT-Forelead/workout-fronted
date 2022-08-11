77987e2d29d1ef2a873cab06275cf3b22abcd40d
Surojiddin <isurojiddin@gmail.com>
2022-08-11 18:46:16 +0500

    messages report front-end

--- src/components/pages/admin/Messages/MessagesInfo.vue
+++ src/components/pages/admin/Messages/MessagesInfo.vue
@@ -1,15 +1,23 @@
 <template>
-  <tr v-for="(msg, index) in messages" :key="index"
-      class="text-gray-700 text-md dark:text-gray-300 dark:bg-gray-800">
+  <tr v-for="(msg, index) in messages" :key="index" class="text-gray-700 text-md dark:text-gray-300 dark:bg-gray-800">
+    <td class="px-4 py-3 whitespace-nowrap">
+      <div class="flex items-center">
+        <div class="inline-block w-10 h-10 p-1 mr-3 border rounded-full shadow border-gray-50 dark:border-gray-600 bg-slate-300 dark:bg-gray-800 text-slate-500 dark:text-gray-500">
+          <UserBoldIcon class="w-full h-full" />
+        </div>
+        <div>
+          <p class="font-semibold text-gray-900 dark:text-gray-300">{{ msg?.user.firstname + " " + msg?.user.lastname }}</p>
+          <p class="text-sm text-gray-600 dark:text-gray-400">{{ phoneStyle(msg?.user.phone) }}</p>
+        </div>
+      </div>
+    </td>
     <td class="px-4 py-3 whitespace-nowrap">
       <div v-if="msg.member" class="flex items-center">
         <div class="flex items-center justify-center w-10 h-10 mr-3 border rounded-full border-gray-50">
-          <img class="object-cover w-full h-full rounded-full"
-               :src="URL + '/member/image/' + msg?.member?.image" alt="#"/>
+          <img class="object-cover w-full h-full rounded-full" :src="URL + '/member/image/' + msg?.member?.image" alt="#"/>
         </div>
         <div>
-          <p class="font-semibold text-gray-900 dark:text-gray-300">
-            {{ msg?.member?.firstname + " " + msg?.member?.lastname }}</p>
+          <p class="font-semibold text-gray-900 dark:text-gray-300">{{ msg?.member?.firstname + " " + msg?.member?.lastname }}</p>
           <p class="text-sm text-gray-600 dark:text-gray-400">{{ phoneStyle(msg?.member?.phone) }}</p>
         </div>
       </div>
@@ -39,11 +47,11 @@
   </tr>
 </template>
 <script setup>
-import UserBoldIcon from '../../../assets/icons/UserBoldIcon.vue'
-import CalendarBlankIcon from '../../../assets/icons/CalendarBlankIcon.vue'
+import UserBoldIcon from '../../../../assets/icons/UserBoldIcon.vue'
+import CalendarBlankIcon from '../../../../assets/icons/CalendarBlankIcon.vue'
 import {toRefs, ref} from "vue";
 import InfiniteLoading from "v3-infinite-loading";
-import {phoneStyle, formatDate} from "../../../utils/utils.js";
+import {phoneStyle, formatDate} from "../../../../utils/utils.js";
 
 const props = defineProps({
   messages: {type: Array, required: true},
